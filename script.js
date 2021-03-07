521b8effdfbc7308cf1e3e2eb50f03b0c1a8f587
matteo-d <matteodrouin33@gmail.com>
2021-03-07 14:35:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// Tableau avec Objet 
+
+const quizData = [
+    {
+        question: 'Qui a les meilleurs fs flip',
+        a: 'Reynodls',
+        a:'Muska', 
+        a:'Romero',
+        a:'Puig',
+        correct: a
+    },
+    {
+        question: 'best Mc twist',
+        a: 'Reynodls',
+        a:'Muska', 
+        a:'Romero',
+        a:'Puig',
+        correct: d
+    },
+    {
+        question: 'Qmeilleur',
+        a: 'Reynodls',
+        a:'Muska', 
+        a:'Romero',
+        a:'Puig',
+        correct: c
+    },
+    {
+        question: 'Qui a lesdc meilleurs fs flip',
+        a: 'Reynodls',
+        a:'Muska', 
+        a:'Romero',
+        a:'Puig',
+        correct: b
+    }
+]
