8fac9b7e1d08d939c2bf694cf6ed15410632055f
matteo-d <matteodrouin33@gmail.com>
2021-03-07 15:48:51 +0100

    Reste à faire afficher score, empecher le submit si aucune question n'est cochée

--- script.js
+++ script.js
@@ -1,36 +1,79 @@
-// Tableau avec Objet 
+// Tableau contenant des Objet 
 
 const quizData = [
+// Chaque objet contient une question, 4 posibilité de réponse et la réponse correcte
     {
-        question: 'Qui a les meilleurs fs flip',
-        a: 'Reynodls',
-        a:'Muska', 
-        a:'Romero',
-        a:'Puig',
+        question: 'Qui est la plus bellisima ?',
+        a:'Julie',
+        b:'Angelina Joli', 
+        c:'Robert',
+        d:'Mattéo',
         correct: a
     },
     {
-        question: 'best Mc twist',
-        a: 'Reynodls',
-        a:'Muska', 
-        a:'Romero',
-        a:'Puig',
+        question: "Qui est la plus mim's ?",
+        a:'Julie saoul',
+        b:'Julie qui dort', 
+        c:'Julie qui est saoulée',
+        d:'Julie TOUT LE TEMPS ! ',
         correct: d
     },
     {
-        question: 'Qmeilleur',
-        a: 'Reynodls',
-        a:'Muska', 
-        a:'Romero',
-        a:'Puig',
+        question: 'Qui va réussir son permis demain ?',
+        a:'La seule et unique, JULIE',
+        b:'Alain prost', 
+        c:'Ayrton Senna',
+        d:'Mickael Jackson',
         correct: c
     },
     {
-        question: 'Qui a lesdc meilleurs fs flip',
-        a: 'Reynodls',
-        a:'Muska', 
-        a:'Romero',
-        a:'Puig',
+        question: 'Meilleur morceau de la Julie ?',
+        a:'Le menton',
+        b:'Les fesses', 
+        c:'Les oreilles',
+        d:'Le coeur (annnnnnw kro chou)',
         correct: b
     }
-]
+];
+// Pointer les éléments dont le innerText va changer au fil des questions
+a_text = document.getElementById('a_text');
+b_text = document.getElementById('b_text');
+c_text = document.getElementById('c_text');
+d_text = document.getElementById('d_text');
+submit = document.getElementById('submit');
+questionElement = document.getElementById('question');
+// Suivre à quelle question on se trouve 
+
+let currentQuestion = 0;
+
+// Appel fonction quiz (Appeler a chauqe fois que l'on submit)
+loadQuiz();
+   
+function loadQuiz() {
+ // Savoir à quelle question on est dans le tableau d'objet du début
+ let currentQuizData = quizData[currentQuestion];
+ // La question affiché sera 
+ questionElement.innerText = currentQuizData.question;
+// Réponses possibles affichées 
+ a_text.innerText = currentQuizData.a;
+ b_text.innerText = currentQuizData.b;
+ c_text.innerText = currentQuizData.c;
+ d_text.innerText = currentQuizData.d;
+ // Passage au prochain objet du tableau quizData
+}
+
+// Au clic sur le bouton submit 
+submit.addEventListener('click',() => {
+    // incrémentation current question
+    currentQuestion++; 
+    //  
+    if(currentQuestion < quizData.length ) {
+        // puis relance le la fonction load quiz avec le prochain objet
+    loadQuiz();
+    }
+    else {
+        //Montre le score au test 
+        alert('tu as finis le test, ton score est de .....')
+    }
+    
+}); 
