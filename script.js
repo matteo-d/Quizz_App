70a3b0d885d57cb2d218258f46aee3f910710f27
matteo-d <matteodrouin33@gmail.com>
2021-03-08 14:24:26 +0100

    Pb incrémentation score réglé,

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const quizData = [
     b: "Angelina Joli",
     c: "Kate moss",
     d: "Les copines de Ju'",
-    correct: a,
+    correct: "a",
   },
   {
     question: "Qui est la plus mim's ?",
@@ -16,7 +16,7 @@ const quizData = [
     b: "Julie qui dort",
     c: "Julie grochon ",
     d: "Julie, TOUT LE PUTAIN DE TEMPS ! ",
-    correct: d,
+    correct: "d",
   },
   {
     question: "Qui va réussir son permis demain ?",
@@ -24,7 +24,7 @@ const quizData = [
     b: "Alain prost",
     c: "Ayrton Senna",
     d: "Mickael Jackson",
-    correct: a,
+    correct: "a",
   },
   {
     question: "Meilleur morceau de la Julie ?",
@@ -32,36 +32,36 @@ const quizData = [
     b: "Les fesses",
     c: "Les oreilles",
     d: "Le coeur (annnnnnw kro chou)",
-    correct: d,
+    correct: "d",
   },
 ];
 
 // Pointer les éléments dont le innerText va changer au fil des questions
-a_text = document.getElementById("a_text");
-b_text = document.getElementById("b_text");
-c_text = document.getElementById("c_text");
-d_text = document.getElementById("d_text");
-submit = document.getElementById("submit");
-questionElement = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submit = document.getElementById("submit");
+const questionElement = document.getElementById("question");
+const answersElements = document.querySelectorAll(".answer");
+const quiz = document.getElementById("quiz");
+
 // Suivre à quelle question on se trouve
-let currentQuestion = 0;
+let currentQuiz = 0;
 // point de départ du score qui s'incrémente si réponse est vraie
 let score = 0;
 // Pointe tout les radios button des reponses
-const answersEls = document.querySelectorAll(".answer");
-// Pointe ver quiz ???
-const quiz = document.getElementById("quiz");
 
 // Appel fonction quiz (Appeler a chauqe fois que l'on submit)
 loadQuiz();
-// fonction charge un quizz
+// fonction charge un quizz 
 function loadQuiz() {
   deselectAnswers();
   // Savoir à quelle question on est dans le tableau d'objet du début
-  let currentQuizData = quizData[currentQuestion];
+  const currentQuizData = quizData[currentQuiz];
   // La question affiché sera
   questionElement.innerText = currentQuizData.question;
-  // Les Réponses possibles affichées
+  // Les Réponses  affichées seront 
   a_text.innerText = currentQuizData.a;
   b_text.innerText = currentQuizData.b;
   c_text.innerText = currentQuizData.c;
@@ -71,7 +71,7 @@ function loadQuiz() {
 function getSelected() {
   let answer = undefined;
 
-  answersEls.forEach((answerEl) => {
+  answersElements.forEach((answerEl) => {
     if (answerEl.checked) {
       answer = answerEl.id;
     }
@@ -82,10 +82,11 @@ function getSelected() {
 
 // Fonction désélectionné un reponse
 function deselectAnswers() {
-  answersEls.forEach((answerEl) => {
+  answersElements.forEach((answerEl) => {
     answerEl.checked = false;
   });
 }
+
 
 // Au clic sur le bouton submit lance cette fonction
 submit.addEventListener("click", () => {
@@ -93,21 +94,20 @@ submit.addEventListener("click", () => {
   // Si une reponse existe on veut ...
   if (answer) {
     // Si la réponse utlisateur = la bonne réponse alors ...
-    if (answer === quizData[currentQuestion].correct) {
+    if (answer === quizData[currentQuiz].correct) {
       // Incrémenter le score de 1
       score++;
     }
 
     // Passer à la prochaine question..
-    currentQuestion++;
+    currentQuiz++;
     // Et si on est pas à la fin du quizz
-    if (currentQuestion < quizData.length) {
+    if (currentQuiz < quizData.length) {
       // puis relance le la fonction load quiz avec le prochain objet
       loadQuiz();
       // Sinon lancer l'alerte
     } else {
       //Montre le score au test
-      window.alert('Attention ce quizz est à la limite du raisonnable quant aux normes sur la canaraderie')
       quiz.innerHTML = `
       <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
       
